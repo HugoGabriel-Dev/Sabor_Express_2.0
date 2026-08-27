989fc012927467fc2075e70d64aca5c371502cf2
Hugo <hg116518@gmail.com>
2026-08-27 15:16:32 -0300

    classes

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 class Restaurante:
-    nome: str
-    categoria: str
-    ativo: True
+    restaurantes = []
+    def __init__(self, nome, categoria):
+        self.nome = nome
+        self.categoria = categoria
+        self.ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_praca = Restaurante("Sabor Express", "Italiana")
+    def __str__(self):
+        return f'Restaurante: {self.nome}, Categoria: {self.categoria}, Ativo: {self.ativo}'
+
+    def listar_restaurantes():
+        for restaurante in Restaurante.restaurantes:
+            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+
+restaurante = Restaurante('Restaurante A', 'Italiana')
+restaurante2 = Restaurante('Restaurante B', 'Chinesa')
+
+Restaurante.listar_restaurantes()
